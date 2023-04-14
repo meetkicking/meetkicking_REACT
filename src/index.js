e352c6fb51bcae97c7ba426cb842a84afddef9aa
unkno <unknown14584@mail.ru>
2023-04-14 21:03:14 +0300

    первый раз с React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+//root.render(
+  //React.createElement("h1", null, "Hello, React!")
+//)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div title="DoggO">
+    <h1>Hello</h1>
+    <h2>
+      <mark>React</mark>
+      </h2>
+  </div>
+)
